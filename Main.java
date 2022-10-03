69fbc592cc70ff808619f10fa6fa8a8429f2ece5
Sean "Jesus" Cohmer <93715513+aprunge@users.noreply.github.com>
2022-10-03 13:10:58 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,302 @@
+public class Main {
+
+    static void a(boolean caps){
+        String a = "a";
+
+        if (caps == true){
+            System.out.print(a.toUpperCase());
+        } else{
+            System.out.print(a);
+        }
+    }
+
+    static void b(boolean caps){
+        String b = "b";
+        
+        if (caps == true){
+            System.out.print(b.toUpperCase());
+        } else{
+            System.out.print(b);
+        }
+    }
+
+    static void c(boolean caps){
+        String c = "c";
+
+        if (caps == true){
+            System.out.print(c.toUpperCase());
+        } else{
+            System.out.print(c);
+        }
+    }
+
+    static void d(boolean caps){
+        String d = "d";
+
+        if (caps == true){
+            System.out.print(d.toUpperCase());
+        } else{
+            System.out.print(d);
+        }
+    }
+
+    static void e(boolean caps){
+        String e = "e";
+
+        if (caps == true){
+            System.out.print(e.toUpperCase());
+        } else{
+            System.out.print(e);
+        }
+    }
+
+    static void f(boolean caps){
+        String f = "f";
+
+        if (caps = true){
+            System.out.print(f.toUpperCase());
+        } else{
+            System.out.print(f);
+        }
+    }
+
+    static void g(boolean caps){
+        String g = "g";
+
+        if (caps == true){
+            System.out.print(g.toUpperCase());
+        } else{
+            System.out.print(g);
+        }
+    }
+
+    static void h(boolean caps){
+        String h = "h";
+
+        if (caps == true){
+            System.out.print(h.toUpperCase());
+        } else{
+            System.out.print(h);
+        }
+    }
+
+    static void i(boolean caps){
+        String i = "i";
+
+        if (caps == true){
+            System.out.print(i.toUpperCase());
+        } else{
+            System.out.print(i);
+        }
+    }
+
+    static void j(boolean caps){
+        String j = "j";
+
+        if (caps == true){
+            System.out.print(j.toUpperCase());
+        } else{
+            System.out.print(j);
+        }
+    }
+
+    static void k(boolean caps){
+        String k = "k";
+
+        if (caps == true){
+            System.out.print(k.toUpperCase());
+        } else{
+            System.out.print(k);
+        }
+    }
+
+    static void l(boolean caps){
+        String l = "l";
+
+        if (caps == true){
+            System.out.print(l.toUpperCase());
+        } else{
+            System.out.print(l);
+        }
+    }
+
+    static void m(boolean caps){
+        String m = "m";
+
+        if (caps == true){
+            System.out.print(m.toUpperCase());
+        } else{
+            System.out.print(m);
+        }
+    }
+
+    static void n(boolean caps){
+        String n = "n";
+
+        if (caps == true){
+            System.out.print(n.toUpperCase());
+        } else{
+            System.out.print(n);
+        }
+    }
+
+    static void o(boolean caps){
+        String o = "o";
+
+        if (caps == true){
+            System.out.print(o.toUpperCase());
+        } else{
+            System.out.print(o);
+        }
+    }
+
+    static void p(boolean caps){
+        String p = "p";
+
+        if (caps == true){
+            System.out.print(p.toUpperCase());
+        } else{
+            System.out.print(p);
+        }
+    }
+
+    static void q(boolean caps){
+        String q = "q";
+
+        if (caps == true){
+            System.out.print(q.toUpperCase());
+        } else{
+            System.out.print(q);
+        }
+    }
+
+    static void r(boolean caps){
+        String r = "r";
+
+        if (caps == true){
+            System.out.print(r.toUpperCase());
+        } else{
+            System.out.print(r);
+        }
+    }
+
+    static void s(boolean caps){
+        String s = "s";
+
+        if (caps == true){
+            System.out.print(s.toUpperCase());
+        } else{
+            System.out.print(s);
+        }
+    }
+
+    static void t(boolean caps){
+        String t = "t";
+
+        if (caps == true){
+            System.out.print(t.toUpperCase());
+        } else{
+            System.out.print(t);
+        }
+    }
+
+    static void u(boolean caps){
+        String u = "u";
+
+        if (caps == true){
+            System.out.print(u.toUpperCase());
+        } else{
+            System.out.print(u);
+        }
+    }
+
+    static void v(boolean caps){
+        String v = "v";
+
+        if (caps == true){
+            System.out.print(v.toUpperCase());
+        } else{
+            System.out.print(v);
+        }
+    }
+
+    static void w(boolean caps){
+        String w = "w";
+
+        if (caps == true){
+            System.out.print(w.toUpperCase());
+        } else{
+            System.out.print(w);
+        }
+    }
+
+    static void x(boolean caps){
+        String x = "x";
+
+        if (caps == true){
+            System.out.print(x.toUpperCase());
+        } else{
+            System.out.print(x);
+        }
+    }
+
+    static void y(boolean caps){
+        String y = "y";
+
+        if (caps == true){
+            System.out.print(y.toUpperCase());
+        } else{
+            System.out.print(y);
+        }
+    }
+
+    static void z(boolean caps){
+        String z = "z";
+
+        if (caps == true){
+            System.out.print(z.toUpperCase());
+        } else{
+            System.out.print(z);
+        }
+    }
+
+    static void space(){
+        System.out.print(" ");
+    }
+
+    static void period(){
+        System.out.print(".");
+    }
+    public static void main(String[] args){
+        c(true);
+        o(false);
+        o(false);
+        p(false);
+        e(false);
+        r(false);
+        space();
+        i(false);
+        s(false);
+        space();
+        s(false);
+        o(false);
+        space();
+        g(false);
+        a(false);
+        y(false);
+        period();
+        space();
+        a(true);
+        n(false);
+        d(false);
+        space();
+        s(false);
+        o(false);
+        space();
+        a(false);
+        m(false);
+        space();
+        i(true);
+        period();
+    }
+}
